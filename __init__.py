0aa1e0fa5bfa06693650e8116bd1bc88232365c8
Åke Forslund <ake.forslund@gmail.com>
2020-06-08 07:26:57 +0200

    Minor clean-up

--- __init__.py
+++ __init__.py
@@ -3,23 +3,25 @@ from mycroft.skills.core import FallbackSkill
 
 class MeaningFallback(FallbackSkill):
     """
-        A Fallback skill to answer the question about the
-        meaning of life, the universe and everything.
+    A Fallback skill to answer the question about the
+    meaning of life, the universe and everything.
     """
-    def __init__(self):
-        super(MeaningFallback, self).__init__(name='Meaning Fallback')
-
     def initialize(self):
-        """
-            Registers the fallback skill
-        """
+        """Registers the fallback skill."""
+        # Warning:
+        # This sets the fallback priority to 1, making it happen before
+        # all other fallbacks, even padatious intents.
+        # This is good for this example but BAD in general,
+        # a fallback prio between 11 and 89 is good for most skills.
+
         self.register_fallback(self.handle_fallback, 1)
+
         # Any other initialize code goes here
 
     def handle_fallback(self, message):
-        """
-            Answers question about the meaning of life, the universe
-            and everything.
+        """Answers the ultimate question
+
+        What is the meaning of life, the universe and everything.
         """
         utterance = message.data.get("utterance")
 
@@ -30,22 +32,14 @@ class MeaningFallback(FallbackSkill):
         universe = self.dialog_renderer.render('universe')
         everything = self.dialog_renderer.render('everything')
 
-        if what in utterance \
-            and meaning in utterance \
-            and (life in utterance \
-                or universe in utterance \
-                or everything in utterance):
+        if (what in utterance and
+                meaning in utterance and
+                (life in utterance or universe in utterance
+                    or everything in utterance)):
             self.speak('42')
-            return True # Indicate that the utterance was handled
+            return True  # Indicate that the utterance was handled
         else:
             return False
-
-    def shutdown(self):
-        """
-            Remove this skill from list of fallback skills.
-        """
-        self.remove_fallback(self.handle_fallback)
-        super(MeaningFallback, self).shutdown()
 
 
 def create_skill():
